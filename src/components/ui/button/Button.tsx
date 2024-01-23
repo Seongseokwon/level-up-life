fb19e7224e853cfc5635b57c06e2c3d07dae84b0
SeongSeokwon <pam9411@naver.com>
2024-01-23 19:49:38 +0900

    button xsm 사이즈 추가

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -2,12 +2,14 @@ import {ReactNode} from "react";
 import styles from './Button.module.scss';
 
 type ButtonType = 'button' | 'reset' | 'submit';
-type ButtonSize = 'sm' | 'md' | 'lg' | 'xlg'
+type ButtonSize = 'xsm' | 'sm' | 'md' | 'lg' | 'xlg'
 type ButtonColorStyle = 'default' | 'primary' | 'error';
+
 interface ButtonStyleProps {
     size?: ButtonSize
     colorStyle?: ButtonColorStyle
 }
+
 interface ButtonOptionProps {
     id?: string,
     type: ButtonType,
@@ -15,7 +17,9 @@ interface ButtonOptionProps {
     children: ReactNode,
     onClick?: () => void
 }
-interface ButtonProps extends ButtonOptionProps, ButtonStyleProps {}
+
+interface ButtonProps extends ButtonOptionProps, ButtonStyleProps {
+}
 
 
 export default function Button({type, children, size, colorStyle, onClick}: ButtonProps) {
@@ -23,7 +27,7 @@ export default function Button({type, children, size, colorStyle, onClick}: Butt
     const buttonClasses = `
         ${styles.button} 
         ${size ? styles[size] : ''}
-        ${colorStyle? styles[colorStyle] : ''}
+        ${colorStyle ? styles[colorStyle] : ''}
     `;
 
     return (
