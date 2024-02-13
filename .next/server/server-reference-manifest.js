b107b6ab06d932b3f26abac70c26d9e7acf83f74
SeongSeokwon <pam9411@naver.com>
2024-02-13 15:38:47 +0900

    reset

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ApEC5RejMlpaPhX6D0VxnkHbANdwBjdrftpaWPxx1cM=\"\n}"
--- src/components/bottomNavigation/BottomNavigation.tsx
+++
@@ -1,5 +0,0 @@
-export default function BottomNavigation() {
-    return <nav>
-        bottom navigation
-    </nav>
-}
